5b24d89e6ecefb76cc66e02ff971e3ca20e3a3ea
GreenArrow2017 <34080744+GreenArrow2017@users.noreply.github.com>
2021-03-08 11:17:59 +0800

    add template

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,31 @@
 import json
 
-def read_dataset(filename):
-    filename_string = '../data/' + filename
-    with open(filename_string, 'r') as load_f:
-        load_dict = json.load(load_f)
-        return load_dict
+def read_json(filename):
+    filename = '../data/' + filename
+    with open(filename, 'r') as f:
+        json_data = json.load(f)
+    return json_data
+
+
+def read_math23k_json(filename):
+    filename = '../data/' + filename
+    data_list = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        count = 0
+        string = ''
+        for line in f:
+            count += 1
+            string += line
+            if count % 7 == 0:
+                data_list.append(json.loads(string))
+                string = ''
+    return data_list
+
+
 
 if __name__ == '__main__':
-    file_dict = read_dataset('HMWP/hmwp.json')
+    file_list = read_json("allArith/questions.json")
+    for i in range(len(file_list)):
+        if len(file_list[i]['quants']) == 3:
+            print(file_list[i]['iIndex'], file_list[i]['lEquations'])
+
